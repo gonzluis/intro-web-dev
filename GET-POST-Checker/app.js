e60a0c96675dee5bc3861dc93506ccb6d181b1d2
FearJoy <john.fitzpatrick.v@gmail.com>
2016-02-21 20:55:21 -0500

    Scrapped and restarted. Server running. Landing page works.

---
+++ GET-POST-Checker/app.js
@@ -0,0 +1,68 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Port to run server on
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+var port = 3000;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Require express and get it running
+// Require body-parser
+// Require handlebars and set it up
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+var express = require("express");
+var bodyParser = require("body-parser");
+var handlebars = require ("express-handlebars").create({defaultLayout:'main'});
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// INIT express enngine
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+var app = express();
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', port);
+var options = { root: __dirname + '/' };
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Set express to use middleware(bodyParser)
+//    parse application/x-www-form-urlencoded = false
+//    parse application/json = true
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Initial landing page from index.handlebars
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+app.get('/', function(req,res,next) {
+  res.render('index');      // index.handlebars
+});
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Error Handling
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+app.use(function(req, res){
+    res.status(404);
+    res.render("404");
+});
+
+app.use(function(err, req, res, next){
+    console.log(err.stack);
+    res.status(500);
+    res.render("500");
+});
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Set server to listen for requests on var port
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+app.listen(port, function() {
+  console.log('Started on port: ' + port + ', Ctrl C to end.');
+});
