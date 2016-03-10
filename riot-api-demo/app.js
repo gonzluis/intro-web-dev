e6c22936ebedd98e0f11daf63b95312560a843ad
FearJoy <john.fitzpatrick.v@gmail.com>
2016-03-09 21:39:24 -0500

    Set up, IDE tuning

--- riot-api-demo/app.js
+++ riot-api-demo/app.js
@@ -17,3 +17,29 @@ var options = { root: __dirname + '/' };
 // Make express use body-parser as middleware
 app.use(bodyParser.urlencoded({ extended:false}));
 app.use(bodyParser.json());
+
+// Landing page, via main.hbs
+app.get('/', function(req,res) {
+    res.render('main');
+});
+
+$(function() {
+    // Grab the template script
+    var theTemplateScript = $("#address-template").html();
+
+    // Compile the template
+    var theTemplate = Handlebars.compile(theTemplateScript);
+
+    // Define our data object
+    var context={
+        "city": "London",
+        "street": "Baker Street",
+        "number": "221B"
+    };
+
+    // Pass our data to the template
+    var theCompiledHtml = theTemplate(context);
+
+    // Add the compiled html to the page
+    $('.content-placeholder').html(theCompiledHtml);
+});
