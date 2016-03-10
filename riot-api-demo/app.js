46b02c916e269411aba300943ff5c1cb46a48c06
FearJoy <john.fitzpatrick.v@gmail.com>
2016-03-09 21:24:11 -0500

    Set up, IDE tuning

--- riot-api-demo/app.js
+++ riot-api-demo/app.js
@@ -1,3 +1,19 @@
-/**
- * Created by JohnFitzpatrick on 3/8/16.
- */
+
+// Port to be used
+var port = 3000;
+
+// Require stuff
+var express = require("express");
+var bodyParser = require("body-parser");
+var handlebars = require("express-handlebars").create({defaultLayout:'main'});
+
+// Init express
+var app = express();
+app.engine('handlebars', handlebars.engine);
+app.set('port', port);
+var options = { root: __dirname + '/' };
+
+
+// Make express use body-parser as middleware
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
